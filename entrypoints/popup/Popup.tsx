fc4ca16de57a6788bc5d943c55af0022ff475820
Will Ginsberg <will.j.ginsberg@gmail.com>
2024-10-10 14:54:49 -0500

    better loading for options

--- entrypoints/popup/Popup.tsx
+++ entrypoints/popup/Popup.tsx
@@ -3,7 +3,7 @@ import reactLogo from '@/assets/react.svg';
 import wxtLogo from '/wxt.svg';
 import Auth from '@/components/auth';
 import RepoMultiselect from '@/components/repo-multiselect';
-import { Anchor, Button, Overlay, Paper, Space, Text } from '@mantine/core';
+import { Anchor, Button, Loader, Overlay, Paper, Skeleton, Space, Text } from '@mantine/core';
 import { STORAGE } from '@/constants';
 import { useStorage } from '@plasmohq/storage/hook';
 import Empty from './Empty';
@@ -11,9 +11,20 @@ import { useGithubWorkflowRuns } from "@/hooks/useGithubWorkflowRuns"
 import WorkflowRunFeed from '@/components/workflow-run-feed';
 
 export default function Popup() {
-  const { githubUser } = useGithubUser()
-  const [selectedRepos, setSelectedRepos] = useStorage<string[]>(STORAGE.SELECTED_REPOS, [])
+  const { githubUser, isGithubUserLoading } = useGithubUser()
+  const [selectedRepos, setSelectedRepos, { isLoading: isSelectedReposLoading }] = useStorage<string[]>(STORAGE.SELECTED_REPOS, [])
   const { githubWorkflowRuns, isWorkflowRunListLoading, githubWorkflowError, isPaused, unPause } = useGithubWorkflowRuns(selectedRepos)
+
+  if (isSelectedReposLoading || isGithubUserLoading) {
+    return (
+      <div className="p-2 flex flex-col items-center">
+        <div className="max-w-md w-full">
+          <Anchor href="options.html">Options →</Anchor>
+          <Skeleton w="100%" h={8} />
+        </div>
+      </div>
+    )
+  }
 
   const showSkeleton = selectedRepos.length > 0 && isWorkflowRunListLoading
 
